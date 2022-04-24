1e5852826b5a517504e14b9c2375ffbf784a0c71
Chs88 <faskertikr@gmail.com>
2022-04-24 03:52:11 +0100

    Added timestamp and error handling

--- main.py
+++ main.py
@@ -1,47 +1,62 @@
-import variables
+from datetime import datetime
 import os
 import subprocess as sb
 import logging
 
 
-## configure directories
-
-os.chdir("/home/chs88/Desktop/Practice_Projects")
-wdir = os.getcwd() ## working directory
-# ddir =  ## destination directory
-
 
 ## Configure logging
 
 LOG_FORMAT = "%(asctime)s - %(message)s"
-logging.basicConfig(filename=os.path.join(wdir, "BACKUP/backuplog"), level=logging.DEBUG, format=LOG_FORMAT)
+logging.basicConfig(filename="/home/chs88/scripts_backup/backuplog.txt", level=logging.DEBUG, format=LOG_FORMAT)
 logger = logging.getLogger()
+
+
+## Configure timestamp
+# dd/mm/YY H:M:S
+now = datetime.now()
+date_string = now.strftime("%d-%m-%Y-%H:%M")
+
+
+## configure directories and file names
+
+#check if the folder exists
+def wdirExists():
+    try:
+        os.chdir("/home/chs88/Desktop/Practice_Projects")
+    except:
+        logging.error("Working directory doesn't exist")
+    else:
+        os.chdir("/home/chs88/Desktop/Practice_Projects")
+        wdir = os.getcwd() ## working directory
+        return wdir
+
+# destination directory
+ddir = "/home/chs88/scripts_backup/" 
+#configuring the filename for the backup tar file
+filename = f"{ddir}backup-{date_string}"
+
+
 
 
 ## Configure functions for the backup
 
-#Find existing backup file:
 
-def findBackup(name, path):
-    file_name = os.path.join(path, name)
-    exists = os.path.exists(file_name)
-    if exists == True:
-        
-
-    
-
-def createArchive(directory, file_name):
-    number = 0
+def createArchive(wdir, filename):
     logging.info(f"Backup started on " + wdir)
-    p1 = sb.run(['tar', '-cvf', f"{file_name}{number}",  directory], capture_output=True, text=True,)
-    # logging.info(f"Backing up " + wdir)
-    logging.info("Backup successful.")
+    print(f"Backup started on " + wdir)
+    try:
+        p1 = sb.run(['tar', '-cvf', filename,  wdir], capture_output=True, text=True,)
+        logging.info("Backup successful.")
+    except:
+        logging.error(f"An exception occurred.")
+
     
-
-
 
 
 
 if __name__ == "__main__":
-    createArchive(wdir)
+    createArchive(
+        wdirExists(), filename
+    )
     # pass
